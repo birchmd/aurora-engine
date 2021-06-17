da18158ff711b00ad47c2b40cb2d96c2e724babe
Michael Birch <michael.birch@aurora.dev>
2021-06-17 16:22:41 -0400

    Try to call exit precompiles

--- src/tests/test_block_hash.rs
+++ src/tests/test_block_hash.rs
@@ -20,12 +20,12 @@ fn test_testnet() {
     actix::System::new().block_on(async move {
         let client = create_testnet_client();
 
-        /*let reader = std::fs::File::open("/home/birchmd/.near-credentials/testnet/birchmd.testnet.json").unwrap();
+        let reader = std::fs::File::open("/home/birchmd/.near-credentials/testnet/birchmd.testnet.json").unwrap();
         let json_key: JsonKey = serde_json::from_reader(reader).unwrap();
         let near_signing_key = SigningKey::parse_key(&json_key.account_id, &json_key.private_key);
         let eth_signing_key = secp256k1::SecretKey::parse_slice(&hex::decode("7d640019603753a2449e6b57201ea1e7ee642b287cba417e8e9ba253c7cfa442").unwrap()).unwrap();
         let signing_address = crate::test_utils::address_from_secret_key(&eth_signing_key);
-        let dest_address = Address::from_slice(&hex::decode("Cc5A584F545B2Ca3EbaCc1346556d1f5B82B8fC6").unwrap());
+        /*let dest_address = Address::from_slice(&hex::decode("Cc5A584F545B2Ca3EbaCc1346556d1f5B82B8fC6").unwrap());
 
         //deploy_erc20(&client, &near_signing_key, &eth_signing_key).await;
         mint_erc20(&client, &near_signing_key, &eth_signing_key).await;
@@ -36,8 +36,44 @@ fn test_testnet() {
         println!("{:?}", get_erc20_balance(&client, &near_signing_key, &eth_signing_key, signing_address).await);
         std::thread::sleep(std::time::Duration::from_secs(5));
         println!("{:?}", get_erc20_balance(&client, &near_signing_key, &eth_signing_key, dest_address).await);*/
-        println!("{:?}", call_aurora_ft_balance_of(&client, "aurora").await);
+        println!("AURORA_FT_BALANCE: {:?}", call_aurora_ft_balance_of(&client, "aurora").await);
+        println!("BIRCHMD_FT_BALANCE: {:?}", call_aurora_ft_balance_of(&client, "birchmd.testnet").await);
+        println!("EXIT_ETH_BALANCE: {:?}", call_aurora_view_fn(&client, "get_balance", Address::from_slice(&hex::decode("e9217bc70b7ed1f598ddd3199e80b093fa71124f").unwrap())).await);
+        println!("{:?}_ETH_BALANCE: {:?}", signing_address, call_aurora_view_fn(&client, "get_balance", signing_address).await);
+        println!("{:?}", hex::encode(b"birchmd.testnet"));
+        exit_to_near(&client, &near_signing_key, &eth_signing_key).await;
+        std::thread::sleep(std::time::Duration::from_secs(5));
+        println!("AURORA_FT_BALANCE: {:?}", call_aurora_ft_balance_of(&client, "aurora").await);
+        println!("BIRCHMD_FT_BALANCE: {:?}", call_aurora_ft_balance_of(&client, "birchmd.testnet").await);
+        println!("EXIT_ETH_BALANCE: {:?}", call_aurora_view_fn(&client, "get_balance", Address::from_slice(&hex::decode("e9217bc70b7ed1f598ddd3199e80b093fa71124f").unwrap())).await);
+        println!("{:?}_ETH_BALANCE: {:?}", signing_address, call_aurora_view_fn(&client, "get_balance", signing_address).await);
     });
+}
+
+async fn exit_to_near(client: &JsonRpcClient, near_signing_key: &SigningKey, eth_signing_key: &secp256k1::SecretKey) {
+    let (block_hash, _, nonce) = latest_block_and_nonce(client, near_signing_key).await;
+
+    let address = crate::test_utils::address_from_secret_key(&eth_signing_key);
+    let eth_nonce = call_aurora_view_fn(client, "get_nonce", address).await;
+    let data = b"\x00birchmd.testnet";
+    let tx = crate::transaction::EthTransaction {
+        nonce: eth_nonce,
+        gas_price: Default::default(),
+        gas: 10_000_000.into(),
+        to: Some(Address::from_slice(&hex::decode("e9217bc70b7ed1f598ddd3199e80b093fa71124f").unwrap())),
+        value: crate::types::Wei::new_u64(1),
+        data: Vec::from(data.as_slice()),
+    };
+    let signed_eth_tx = crate::test_utils::sign_transaction(tx, Some(1313161555), eth_signing_key);
+    let near_tx = create_submit_tx(signed_eth_tx, block_hash, nonce, near_signing_key);
+
+    let outcome = send_tx(near_tx, client, near_signing_key).await;
+    let return_value = get_return_value(&outcome);
+    let submit_result = crate::parameters::SubmitResult::try_from_slice(&return_value).unwrap();
+
+    println!("{:?}", outcome);
+    println!("{:?}", submit_result);
+    println!("{:?}", hex::encode(submit_result.result));
 }
 
 async fn deploy_erc20(client: &JsonRpcClient, near_signing_key: &SigningKey, eth_signing_key: &secp256k1::SecretKey) {
